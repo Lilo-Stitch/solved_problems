51b3a3d7bff576f528125e0b5c790e0f895d9b05
Zhuldyz Namazbayeva <40865688+znamazbayeva@users.noreply.github.com>
2020-12-11 13:31:19 +0600

    Create reverse-linked-list.cpp

---
+++ zhuldyz/leetcode/100interviewquestions/reverse-linked-list.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ /* TLE, but i dont know why
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL) return head;
+        ListNode* change = head;
+        ListNode* store = head;
+        while (head->next != NULL) {
+            change = head->next;
+            if (change -> next != NULL) store = change->next;
+            head->next = NULL;
+            change->next = head;
+            head= change;
+        }
+       return store;
+    }
+};
+*/ 
