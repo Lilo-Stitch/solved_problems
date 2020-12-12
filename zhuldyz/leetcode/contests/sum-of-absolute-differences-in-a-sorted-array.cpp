775f05fa0b86b6dc355efbc6bd8c7197500b08ce
Zhuldyz Namazbayeva <40865688+znamazbayeva@users.noreply.github.com>
2020-12-12 21:20:41 +0600

    Tle (help me)

--- zhuldyz/leetcode/contests/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ zhuldyz/leetcode/contests/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -1,1 +1,18 @@
 //now trying
+// TLE
+// O(n^2)
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        vector<int> answ;
+        int sum;
+        for(int i = 0; i< nums.size(); i++) {
+            sum = 0;
+            for(int j = 0; j < nums.size(); j++) {
+                sum += abs(nums[i] - nums[j]); 
+            }
+            answ.push_back(sum);
+        }
+        return answ;
+    }
+};
