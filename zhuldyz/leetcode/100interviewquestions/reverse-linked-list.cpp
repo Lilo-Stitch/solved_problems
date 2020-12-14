abba3f6c8e4a48e9f9cb21df5dec1a67e99c224d
Zhuldyz Namazbayeva <40865688+znamazbayeva@users.noreply.github.com>
2020-12-14 21:00:33 +0600

    accepted O(n)

--- zhuldyz/leetcode/100interviewquestions/reverse-linked-list.cpp
+++ zhuldyz/leetcode/100interviewquestions/reverse-linked-list.cpp
@@ -26,3 +26,17 @@ public:
     }
 };
 */ 
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head ||!(head->next)   ) return head;
+     
+        
+        ListNode* curr = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return curr;
+    }
+
+
