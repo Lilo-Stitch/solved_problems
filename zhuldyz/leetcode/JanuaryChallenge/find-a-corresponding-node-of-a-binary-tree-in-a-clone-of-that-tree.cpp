55354616eeaacc2139a184a8f22b73d2d3c328e2
Zhuldyz Namazbayeva <40865688+znamazbayeva@users.noreply.github.com>
2021-01-03 13:30:22 +0600

    Create find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp

---
+++ zhuldyz/leetcode/JanuaryChallenge/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        queue<TreeNode*> o;
+        o.push(original);
+        
+        queue<TreeNode*> c;
+        c.push(cloned);
+
+        while (!o.empty()) {
+            TreeNode* node_o = o.front();
+            TreeNode* node_c = c.front();
+            o.pop();
+            c.pop();
+            
+            if (node_o == target) {
+                return node_c;   
+            }
+            
+            if (node_o->left) {
+                o.push(node_o->left);
+                c.push(node_c->left);
+            }
+            if (node_o->right) {
+                 o.push(node_o->right);
+                c.push(node_c->right);
+            }
+        }
+        return NULL;
+    }
+    
+};
